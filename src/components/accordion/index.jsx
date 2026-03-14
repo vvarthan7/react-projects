daef21afc624748e443f9d53ee1cb96c1b4a2884
VISHNUVARTHAN U <vvarthan7@gmail.com>
2026-03-14 11:50:15 +0530

    minor fixes

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -3,57 +3,56 @@ import AccordionItem from "./accordionItem";
 import posts from "./data";
 
 const Accordion = () => {
-  const [multiSelect, setMultiSelect] = useState(false);
-  const [selected, setSelected] = useState(false);
+  const [multiSelect, setMutiSelect] = useState(false);
+  const [selected, setSelected] = useState(null);
   const [multiple, setMultiple] = useState([]);
 
   const handleSingleSelect = (id) => {
     setSelected(id === selected ? null : id);
   };
 
-  const handleMultiSelect = (id) => {
-    const copyMutiple = [...multiple];
-    const findIndex = copyMutiple.indexOf(id);
-    if (findIndex === -1) {
-      copyMutiple.push(id);
-    } else {
-      copyMutiple.splice(findIndex, 1);
-    }
-
-    setMultiple(copyMutiple);
+  const handleMultiSelectInput = () => {
+    setMutiSelect((prev) => !prev);
+    setMultiple([]);
+    setSelected(null);
   };
 
+  const handleMultiSelect = (id) => {
+    if (multiple.includes(id)) {
+      setMultiple(multiple.filter((item) => item !== id));
+    } else {
+      setMultiple([...multiple, id]);
+    }
+  };
   return (
-    <div className="w-full justify-start items-start">
-      <div className="flex justify-center my-6">
+    <div className="p-5">
+      <h1 className="text-center text-3xl mb-5">Accordion</h1>
+      <div className="flex justify-center">
         <button
-          className="bg-gray-400 text-white p-2 text-xl cursor-pointer"
-          onClick={() => {
-            setMultiSelect((prev) => !prev);
-            setSelected(null);
-            setMultiple([]);
-          }}
+          onClick={handleMultiSelectInput}
+          className="bg-gray-400 text-white mb-5 p-2 text-left flex justify-between text-xl cursor-pointer"
         >
-          {multiSelect ? "Disable Multi Select" : "Enable Multi Select"}
+          {multiSelect ? "Disable Multiselect" : "Enable Multiselect"}
         </button>
       </div>
-      {posts.map((item) => (
-        <AccordionItem
-          key={item.id}
-          title={item.title}
-          body={item.body}
-          show={
-            multiSelect
-              ? multiple.indexOf(item.id) !== -1
-              : selected === item.id
-          }
-          onClick={
-            multiSelect
-              ? () => handleMultiSelect(item.id)
-              : () => handleSingleSelect(item.id)
-          }
-        />
-      ))}
+      <div className="max-w-4xl mx-auto">
+        {posts.map((post) => (
+          <AccordionItem
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            body={post.body}
+            show={
+              multiSelect ? multiple.includes(post.id) : selected === post.id
+            }
+            onClick={
+              multiSelect
+                ? () => handleMultiSelect(post.id)
+                : () => handleSingleSelect(post.id)
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 };
